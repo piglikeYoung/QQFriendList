6f1a334fe08e3099bdd7c3673314789b3d81ff6d
piglikeyoung <316046510@qq.com>
2015-03-07 20:59:32 +0800

    Initial Commit

---
+++ QQ好友列表/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQ好友列表
+//
+//  Created by piglikeyoung on 15/3/7.
+//  Copyright (c) 2015年 jinheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QQ好友列表/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QQ好友列表
+//
+//  Created by piglikeyoung on 15/3/7.
+//  Copyright (c) 2015年 jinheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
